56a779e670aa009b57e518c477ed78a13345c37b
Daylen Naidoo <daylennaidoo565@gmail.com>
2026-04-14 23:58:41 +0200

    Move db.create_all() outside main block so tables are created on Railway

--- app.py
+++ app.py
@@ -420,8 +420,8 @@ def server_error(e):
     return render_template('error.html', code=500,
                            message="Something went wrong on our end."), 500
 
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
